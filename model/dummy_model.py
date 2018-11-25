ef6b5704fb7ed261c3c215ae78bb30f7554531cc
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2018-11-25 17:07:27 +0300

    x_transformer: add tfidf

--- model/dummy_model.py
+++ model/dummy_model.py
@@ -10,13 +10,13 @@ class DummyModel:
         self.log.info("inited")
 
     def load_train_data(self, x_train, y_train):
-        self.log.info("load x_train size: {0} y_train size: {1}".format(len(x_train), len(y_train)))
+        self.log.info("load x_train size: {0} y_train size: {1}".format(x_train.shape[0], len(y_train)))
         self.x_train = x_train
         self.y_train = y_train
         self.log.info("loaded")
 
     def predict(self, x_to_predict):
-        self.log.info("predict x_to_predict size: {0}".format(len(x_to_predict)))
+        self.log.info("predict x_to_predict size: {0}".format(x_to_predict.shape[0]))
         result = [1]
         self.log.info("predicted")
         return result
--- x_transformer/x_transformer_by_config.py
+++ x_transformer/x_transformer_by_config.py
@@ -3,10 +3,14 @@ sys.path.append("../../base")
 from common import *
 
 from dummy_x_transformer import *
+from tfidf_x_transformer import *
 
 def x_transformer_by_config(config):
     x_transormer_config = config["x_transformer"]
     name = x_transormer_config["name"]
     if (name == "dummy"):
         return DummyXTransformer(x_transormer_config)
+    if (name == "tfidf"):
+        return TfidfXTransformer(x_transormer_config)
     logging.fatal("unknown x transformer name: {0}".format(name))
+    exit(1)
