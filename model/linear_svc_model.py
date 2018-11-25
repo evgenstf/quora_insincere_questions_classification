36593a14565a3e591d227d7994283883ca0a34b4
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2018-11-25 17:32:09 +0300

    common: first working version and kernel script

---
+++ model/linear_svc_model.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append("../../base")
+from common import *
+
+from sklearn.feature_extraction import text
+from sklearn.svm import LinearSVC
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.calibration import CalibratedClassifierCV
+
+class LinearSVCModel:
+    def __init__(self, config):
+        self.log = logging.getLogger("LinearSVCModel")
+        self.name = "linear_svc"
+        self.model = LinearSVC()
+        self.log.info("inited")
+
+    def load_train_data(self, x_train, y_train):
+        self.log.info("load x_train size: {0} y_train size: {1}".format(x_train.shape[0], len(y_train)))
+        self.model.fit(x_train, y_train)
+        self.log.info("loaded")
+
+
+    def predict(self, x_to_predict):
+        self.log.info("predict x_to_predict size: {0}".format(x_to_predict.shape[0]))
+        predictions = 1 /(1 + np.exp(self.model.decision_function(-x_to_predict)))
+        probabilities = []
+        for x in predictions:
+            probabilities.append(0 if x > 0.5 else 1)
+        return probabilities
+
+    def weights(self):
+        return [0]
--- model/model_by_config.py
+++ model/model_by_config.py
@@ -3,10 +3,13 @@ sys.path.append("../../base")
 from common import *
 
 from dummy_model import *
+from linear_svc_model import *
 
 def model_by_config(config):
     model_config = config["model"]
     name = model_config["name"]
     if (name == "dummy"):
         return DummyModel(model_config)
+    if (name == "linear_svc"):
+        return LinearSVCModel(model_config)
     logging.fatal("unknown model name: {0}".format(name))
